cfc12e888bad8f815ea6925e4bd16abec7cc4e3b
Fred Trotter <fred.trotter@gmail.comwq>
2017-12-13 18:54:31 -0600

    jwt s misddle ware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,18 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
+        'careset' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            // \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \CareSet\CareSetJWTAuthClient\Middleware\JWTClientMiddleware::class
+
+        ],
+
         'api' => [
             'throttle:60,1',
             'bindings',
