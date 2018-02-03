6ddf897a8b3d7c2a9291ea62af08b2c8d4385530
Fred Trotter <fred.trotter@gmail.com>
2018-02-02 23:53:51 -0600

    use init.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if ! [ -x "$(command -v composer)" ]; then
-  echo 'Error: composer is not installed. try brew install composer' >&2
-  exit 1
-fi
-cp .env.example .env
-composer update
-php artisan key:generate
