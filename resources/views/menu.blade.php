2ebf5cf0df352b322b62d53c0b1b254cedc5bc0b
Fred Trotter <fred.trotter@gmail.com>
2018-05-23 06:52:20 -0500

    Create menu.blade.php

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="/">DEA Solver</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="https://parenthood.set.care/CareSetReport/ClientImagingTargetUnMatched">GE unmatched</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/DEAtoNPIform/">DEA to NPI form</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/NPItoDEAform/">NPI to DEA form</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href=""></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href=""></a>
+      </li>
+  </div>
+</nav>
