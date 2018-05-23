1035f0a2f7c11b4880c09b2c6994bf02e754bb63
Fred Trotter <fred.trotter@gmail.com>
2018-05-23 06:54:03 -0500

    generic

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/">DEA Solver</a>
+  <a class="navbar-brand" href="/">Brand</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -7,19 +7,14 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="https://parenthood.set.care/CareSetReport/ClientImagingTargetUnMatched">GE unmatched</a>
+        <a class="nav-link" href="">Menu1</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/DEAtoNPIform/">DEA to NPI form</a>
+        <a class="nav-link" href="">Menu2</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/NPItoDEAform/">NPI to DEA form</a>
+        <a class="nav-link" href="">Menu3</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href=""></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href=""></a>
-      </li>
+     </ul>
   </div>
 </nav>
