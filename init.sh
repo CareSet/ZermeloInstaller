7aab5d2651b2af9d8a0db6cd7efa9c8b411f7f7b
Fred Trotter <fred.trotter@gmail.com>
2018-07-24 21:34:31 -0500

    use sudo

--- init.sh
+++ init.sh
@@ -11,16 +11,12 @@ else
     real_user=$(whoami)
 fi
 
-composer update
-cp .env.example .env
-php artisan key:generate
-
 
 #these commands need to run as the regular user...
 sudo -u $real_user composer update
 sudo -u $real_user cp .env.example .env 
 sudo -u $real_user php artisan key:generate
-cp ./templates/ReadMe.template.md README.md
+sudo -u $real_user cp ./templates/ReadMe.template.md README.md
 
 #these commands are the reason that we insist on running with sudo
 chmod 0777 storage -R
