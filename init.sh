4fe8c6b750fddbc9a2af428d52995cb2941867b5
Fred Trotter <fred.trotter@gmail.com>
2018-08-05 08:37:48 -0500

    reorder and use group permissions

--- init.sh
+++ init.sh
@@ -13,11 +13,13 @@ fi
 
 
 #these commands need to run as the regular user...
-sudo -u $real_user composer update
 sudo -u $real_user cp .env.example .env 
+sudo -u $real_user composer update
 sudo -u $real_user php artisan key:generate
 sudo -u $real_user cp ./templates/ReadMe.template.md README.md
 
 #these commands are the reason that we insist on running with sudo
-chmod 0777 storage -R
-chmod 0777 bootstrap/cache -R 
+# These should no longer be needed because the directory should be writeable by careset group
+# and the web server should be a member of the careset group!!
+#chmod 0777 storage -R
+#chmod 0777 bootstrap/cache -R 
